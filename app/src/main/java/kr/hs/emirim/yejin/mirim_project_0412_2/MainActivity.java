f65c05c9a3a479a81ca9d47fcfe2c529a1b8a075
이 예진 <s2108@e-mirim.hs.kr>
2022-04-14 18:58:19 +0900

    직접풀어보기 4-3 입력값이 없을 때류 오류메세지

--- app/src/main/java/kr/hs/emirim/yejin/mirim_project_0412_2/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/yejin/mirim_project_0412_2/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText edit1, edit2;
@@ -30,6 +31,12 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String edit1Str = edit1.getText().toString();
                 String edit2Str = edit2.getText().toString();
+                if(edit1Str.equals("")||edit2Str.equals("")){
+                    Toast.makeText(getApplicationContext(),
+                            "연산자에 필요한 숫자가 입력되지 안았습니다.",
+                            Toast.LENGTH_LONG).show();
+                    return;
+                }
                 int num1 = Integer.parseInt(edit1Str);
                 int num2 = Integer.parseInt(edit2Str);
                 int result = 0;
